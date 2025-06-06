75de24ccf5fa4e2277b4f9e503d44ecec58f8f48
Deepesh Vishwakarma <122160185+Deepesh200@users.noreply.github.com>
2025-06-06 16:25:35 +0530

    Update assignment3_task2.py

--- Assignment 3/assignment3_task2.py
+++ Assignment 3/assignment3_task2.py
@@ -10,13 +10,13 @@ o   Sine of the number (in radians)
 from math import *
 num = float(input("Enter a number :- "))
 if num > 0:
-    sqrt_result = sqrt(num)
-    log_result = log(num)
+    squareroot= sqrt(num)
+    logarithm= log(num)
 else:
    print ("Wrong input Please enter a correct input")
 
 sine_result = sin(num) 
 
-print("Square root :- ", sqrt_result)
-print("logarithm :- ", log_result)
+print("Square root :- ",squareroot)
+print("logarithm :- ",logarithm)
 print("Sine :- ", sine_result)
